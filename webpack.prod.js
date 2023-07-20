6aaa97eada97b0e5eae6a91eef595fe4acea828c
Ernest Bieś <ernestbies@gmail.com>
2023-07-20 13:19:28 +0200

    Added webpack CLI to dependencies.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,40 +1,40 @@
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
     performance: {
-      hints: false
+        hints: false,
     },
     mode: "production",
-    entry: './src/index.js',
+    entry: "./src/index.js",
     output: {
-        filename: './index.js',
-        library: 'react-timer-hook',
+        filename: "./index.js",
+        library: "react-timer-hook",
         libraryTarget: "umd",
-        globalObject: 'typeof self !== \'undefined\' ? self : this',
+        globalObject: "typeof self !== 'undefined' ? self : this",
     },
     module: {
-        rules: [{
-            test: /\.js$/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: [
-                  ['@babel/preset-env', { targets: "defaults" }],
-                  ['@babel/preset-typescript', { allowNamespaces: true }]
-                ]
-              }
-            }
-        }]
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            ["@babel/preset-env", { targets: "defaults" }],
+                            [
+                                "@babel/preset-typescript",
+                                { allowNamespaces: true },
+                            ],
+                        ],
+                    },
+                },
+            },
+        ],
     },
     plugins: [
         new CopyWebpackPlugin({
-            patterns: [
-                { from: './src/index.d.ts', to: './' },
-                ],
+            patterns: [{ from: "./src/index.d.ts", to: "./" }],
         }),
     ],
-    externals: [
-      'react',
-      /^react\/.+$/,
-    ],
-}
+    externals: ["react", /^react\/.+$/],
+};
