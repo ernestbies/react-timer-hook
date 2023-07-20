2aaa1d7bb416f2e6d60790e4e64466bfa99af535
Ernest Bieś <ernestbies@gmail.com>
2023-07-20 12:47:01 +0200

    Expiry validation update.

--- src/utils/Validate.js
+++ src/utils/Validate.js
@@ -1,17 +1,9 @@
 export default class Validate {
-  static expiryTimestamp(expiryTimestamp) {
-    const isValid = (new Date(expiryTimestamp)).getTime() > 0;
-    if (!isValid) {
-      console.warn('react-timer-hook: { useTimer } Invalid expiryTimestamp settings', expiryTimestamp); // eslint-disable-line
+    static expiryTimestamp(expiryTimestamp) {
+        return new Date(expiryTimestamp).getTime() > 0;
     }
-    return isValid;
-  }
 
-  static onExpire(onExpire) {
-    const isValid = onExpire && typeof onExpire === 'function';
-    if (onExpire && !isValid) {
-      console.warn('react-timer-hook: { useTimer } Invalid onExpire settings function', onExpire); // eslint-disable-line
+    static onExpire(onExpire) {
+        return onExpire && typeof onExpire === "function";
     }
-    return isValid;
-  }
 }
